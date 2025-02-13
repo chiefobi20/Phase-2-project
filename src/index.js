7132b9aaa78556fe0eae648340cd312a167160f3
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-02-13 11:27:41 -0500

    Phase 2 success repair

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from './components/About'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 const router = createBrowserRouter([
