f99f0be760b687f311a02b9817aa46d18d5cb91c
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-02-13 11:25:26 -0500

    Project 2 repair

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import About from "./components/About"
 import WorkoutPlanner from './components/WorkoutPlanner';
 import Exercise from './components/Exercise';
@@ -46,7 +26,6 @@ function App() {
     <div className= {darkModeClass}>
       <About num1 = {15} num2 = {5}/>
       <button onClick={toggleDarkMode}>{modeText} Mode</button>
->>>>>>> 8366ca9 (Phase 2read me update)
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 const router = createBrowserRouter([
@@ -34,14 +25,9 @@ root.render(
   //   <h2>This is an Example</h2>
   // </React.Fragment>
 
->>>>>>> 8366ca9 (Phase 2read me update)
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-reportWebVitals();
-=======
 // reportWebVitals();
->>>>>>> 8366ca9 (Phase 2read me update)
