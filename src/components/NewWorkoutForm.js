28fcd1f0dfe4498e7e9037f364856fb8e0a599de
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-02-13 18:02:32 -0500

    Updated Project

--- src/components/NewWorkoutForm.js
+++ src/components/NewWorkoutForm.js
@@ -1,5 +1,26 @@
-function NewWorkoutForm(){
+import { useState } from "react"
 
+function NewWorkoutForm(){
+    const [formData, setFormData] = useState({
+        name: "",
+        image: "",
+        duration: "",
+        description: ""
+    })
+
+    function updateFormData(event){
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    return(
+        <form>
+            <input onChange={updateFormData} type="text" name="name" placeholder="New Name" value={formData.name}/>
+            <input onChange={updateFormData} type="text" name="image" placeholder="New Image" value={formData.image}/>
+            <input onChange={updateFormData} type="text" name="duration" placeholder="New Duration" value={formData.duration}/>
+            <input onChange={updateFormData} type="text" name="description" placeholder="New Description" value={formData.description}/>
+            <input type="submit" value="Add Workout"/>
+        </form>
+    )
 }
 
 export default NewWorkoutForm
