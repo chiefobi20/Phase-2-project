01cb153cda2bd102f362356f9445c2ba98628220
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-02-13 18:32:18 -0500

    Updated lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 function About(){
-
+    return(
+        <h1>The goal of this app is to be your guide and friend while you go on your journey to achieve your fitness goals and level up in life!</h1>
+    )
 }
 
 export default About;
